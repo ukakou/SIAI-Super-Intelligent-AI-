0e815d1e5947c682d48a332a9676ad274b5b96b0
Ulrich L Kakou <ukakou@live.com>
2017-07-03 12:35:40 -0400

    Add files via upload

---
+++ isPrimeCode1.js
@@ -0,0 +1,9 @@
+/*jslint devel: true */
+function primeNumberDetector(n) {
+    "use strict";
+    var commonPrimeNumbers = ['2', '3', '5', '7', '11', '13', '17', '19', '23', '29'], i;
+    
+    for (i = 0; i < commonPrimeNumbers.length; i += 1) {
+        console.log(commonPrimeNumbers[i]);
+    }
+}
